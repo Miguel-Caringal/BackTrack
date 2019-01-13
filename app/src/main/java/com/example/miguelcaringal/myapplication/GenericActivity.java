b818d5874277882b9123e7098794356b7687b27d
Miguel-Caringal <Miguel.Caringal@gmail.com>
2019-01-13 10:03:56 -0500

    Activity

---
+++ app/src/main/java/com/example/miguelcaringal/myapplication/GenericActivity.java
@@ -0,0 +1,13 @@
+package com.example.miguelcaringal.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenericActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generic);
+    }
+}
